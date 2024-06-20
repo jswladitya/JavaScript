7389d1a79d204e1a26b24b8bcc540bd71155fca2
Aditya Jaiswal <jswladitya0403@gmail.com>
2024-06-20 08:44:04 +0000

    update

--- 02_basics/04_objects2.js
+++ 02_basics/04_objects2.js
@@ -1,3 +1,6 @@
+//here we will learn ki object ko singleton and constructer ki help se kaise declare karte he
+
+
 // const tinderUser = new Object() //Singleton object
 const tinderUser = {}; //Non singleton object
 //NOTE: dono hi tarike sahi he object ko declare karne ke
@@ -7,7 +10,7 @@ tinderUser.id = "123abc";
 tinderUser.name = "Aditya";
 tinderUser.isLoggedIn = false;
 
-// console.log(tinderUser); //Array me saare data ptint
+// console.log(tinderUser); //Array me saare data print, eg : {id: "123abc", name:"aditya"}
 
 //nested object
 const regularuser = {
@@ -20,8 +23,8 @@ const regularuser = {
   }
 }
 //Excess
-console.log(regularuser.fullname);//. laga laga ke nesting open kar sakte he
-console.log(regularuser.fullname.userfullname.firstname);
+// console.log(regularuser.fullname);//. laga laga ke nesting open kar sakte he
+// console.log(regularuser.fullname.userfullname.firstname);
 
 //combining multiple objects into 1 object
 const obj1 = {1: "a" , 2: "b"}
@@ -30,7 +33,7 @@ const obj4 = {5: "a" , 6: "b"}
 
 //using spread like we learned in array
 const obj3 = {...obj1,...obj2,...obj4}
-console.log(obj3);
+// console.log(obj3);
 
 
 //database se jab users ka info ata he toh array of object ata he seperated by commas 
@@ -45,16 +48,18 @@ const users = [
         email : "jaiswaladitya116@gmail.com"
     }
 ]
+
 //Excess
-users[1].email //pehle user ki value excess karna he jo ki object he islie .email lika he
-console.log(users[1].email);
+users[1].email //since wo ek aray he so uski values access karne ke lie indexing use karte he, yaha array ki first value access ki ja rahi he
+
+// console.log(users[1].email);
 
 //interesting --> keys ka array
 console.log(tinderUser);
 
-console.log(Object.keys(tinderUser)); 
+// console.log(Object.keys(tinderUser)); 
 //means object ki saari sirf keys(left side wale) dedo jo ki object tinderuser he
-console.log(Object.values(tinderUser)); //left side wale
+// console.log(Object.values(tinderUser)); //left side wale
 console.log(Object.entries(tinderUser)); 
 //saari key-value pairs ka array banega jo ki ek bade array me store hoga
 
@@ -63,6 +68,29 @@ console.log(tinderUser.hasOwnProperty('isLoggedIn')); //true
 
 
 
-//Object destructuring ---> Learn Later
+//Object destructuring 
+const course = {
+  courseName : "Js in hindi",
+  price : "999",
+  courseTeacher : "Aditya"
+}
+//ab in values ko ager use karna ho to kaise kaie karege
+// course.courseTeacher //1st Method , isi me console.log lagake
 
-//JSON API ---> Later Later
+const {courseTeacher : teacher} = course // kaha se values exxtract kar rhe he course mese, and kya value extract karni he wo likhenge curly braket me
+//courseTeacher ko ham kuch bhi naam de sakte he isi ko kehte he destructuring
+console.log(teacher);
+
+
+
+
+
+//JSON API 
+//JSON object jaisa hi dikhta he , object ka naam define jata he but JSON ka naam nhi likha jata & JSON me left side string me likha hota he
+
+// {
+//   "courseName" : "Js in hindi",
+//   "price" : "999",
+//   "courseTeacher" : "Aditya"
+// }
+
