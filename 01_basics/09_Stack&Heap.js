a94569119718c6015b6595319204f20d7b0de19c
Aditya Jaiswal <jswladitya0403@gmail.com>
2024-06-30 16:07:57 +0000

    update

---
+++ 01_basics/09_Stack&Heap.js
@@ -0,0 +1,9 @@
+
+// STACK MEMORY :
+// 1.All primitive types stored in Stack Memory
+// 2.Jab bhi stack memory use hoti he iska mtlb jo bhi variable declare kia he hamne ,hame uska copy milta he 
+
+
+// HEAP MEMORY :
+// 1.All Non primitive types stored in Heap Memory 
+// 2.jo bhi variable ya object heap me jata he toh hume uska reference milta he original value ka , jo bhi change karenge wo original value me change hoga
