7d4c3872438db2f9df8498387fa0ffec44d3075b
Aditya Jaiswal <jswladitya0403@gmail.com>
2024-07-07 09:55:40 +0000

    update

--- 13_classes_and_oop/1_oop.js
+++ 13_classes_and_oop/1_oop.js
@@ -1,3 +1,59 @@
 //Parts of oop --> constructor function, prototypes, classes (new, this keyword kaise aye)
 
 //4 pillars of oop --> Abstraction, Encapsulation, Inheritance, Polymorphism
+
+
+
+//object literal
+const user = {
+    username : "Aditya",
+    loginCount: 5,
+    signedIn: true,
+
+    getUserDetails: function(){
+        // console.log("Got User Details From Database ");
+        // console.log(`username: ${this.username}`);
+        //it tells ki ham current context ki baat kar rahe he
+        
+
+        //sirf this print karaye toh current context dedega 
+        // it is like function ko bolna ki yaha se ie.., is context se ja kar values lo
+        // console.log(this);
+
+    }
+}
+
+// console.log(user.username);
+// console.log(user.getUserDetails());
+
+
+//NOTE : if this ham global context me print karaye node env me toh empty object aayega & if browser me ja ke same cheez kare toh waha window as an global object ayega
+// console.log(this); //{}
+
+
+
+
+//constructor function
+
+// const promiseOne = new Promise()
+// const date = new Date()
+// NOTE: new keyword is actually a constructor function, constructor function allows to make multiple instances from 1 object literal
+//new keyword makes new context & isi ko constructor function bolte he
+// NOTE: Working Mechanism ::: jaise hi new keyword use karte he to ek empty object create hota he jisko instance bola jata he & ek constructor function call hota he new keyword ke karan , phir this keyword jitne bhi arguments likhe he wo sab uske ander inject ho jate he & then finaly wo function ke ander mil jate he
+
+
+//example
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    // return this //ye bydefault return hota hi he likhne ki zarurat nahi he
+}
+
+const userOne = new User("aditya", 12 , true)
+
+const userTwo = new User("Chaiaurcode", 11, false)
+console.log(userOne);
+console.log(userTwo);
+
