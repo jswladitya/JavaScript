306c7ad240f8682478e50c61c85bf6f16c7a87fe
jswladitya <jswladitya0403@gmail.com>
2024-07-18 23:19:32 +0530

    update

--- 13_classes_and_oop/5_myClasses.js
+++ 13_classes_and_oop/5_myClasses.js
@@ -1,7 +1,22 @@
 class User {
+    // jaise hi class se ek object create hoga means jaise hi wo new keyword kaam me loge tabhi constructor apne app call ho jata he
     constructor(username, email, password){
         this.username= username;
         this.email= email;
         this.password=password;
     }
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
 }
+
+const chai = new User("chai", "chai@gmail.com", "123")
+
+console.log(chai.encryptPassword());
+console.log(chai.changeUsername());
+
+//Here, exactly what is happening behind the scene
