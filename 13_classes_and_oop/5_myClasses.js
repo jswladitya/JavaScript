4dd8a1b1bff34e0e16295db249ae31a6f286e878
jswladitya <jswladitya0403@gmail.com>
2024-07-19 14:45:29 +0530

    finally learned classes

--- 13_classes_and_oop/5_myClasses.js
+++ 13_classes_and_oop/5_myClasses.js
@@ -19,4 +19,25 @@ const chai = new User("chai", "chai@gmail.com", "123")
 console.log(chai.encryptPassword());
 console.log(chai.changeUsername());
 
+
+
+
 //Here, exactly what is happening behind the scene
+//sabse paehle ek function hota 
+function User(username, email, password){
+    this.username= username;
+        this.email= email;
+        this.password=password;
+}
+
+User.prototype.encryptPassword = function(){
+     return `${this.password}abc`
+}
+
+User.prototype.changeUsername= function(){
+    return `${this.username.toUpperCase()}`
+}
+const tea = new User("tea", "tea@gmail.com", "123")
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
+
