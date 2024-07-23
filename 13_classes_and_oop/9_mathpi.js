11ae8e36f845dd64a04a6b6890bd253b3b4ad815
jswladitya <jswladitya0403@gmail.com>
2024-07-23 20:51:46 +0530

    update

---
+++ 13_classes_and_oop/9_mathpi.js
@@ -0,0 +1,8 @@
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+//here aaap ne toh overwrte kardi par wo overwrite hui nahi
+
+//So now 
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
