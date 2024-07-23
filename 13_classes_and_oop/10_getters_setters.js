095a0711c9ac80445a7cd674cc8810d94b21f603
jswladitya <jswladitya0403@gmail.com>
2024-07-23 21:07:42 +0530

    update

---
+++ 13_classes_and_oop/10_getters_setters.js
@@ -0,0 +1,26 @@
+//we use getters & setters for security purposes means we dont want ki koi hamara confedentail chheze access kare
+
+class User {
+    constructor(email, password){
+        this.email= email;
+        this.password = password
+    }
+    
+    get email(){
+        return this._email.toUpperCase()
+    }
+
+    set email(value){
+       this._email = value
+    }
+
+    get password(){
+        return `${this._password}aditya`
+    }
+
+    set password(value){
+        this._password=value
+    }
+}
+const aditya = new User("aditya@123", "123")
+console.log(aditya.password);
