89b615917fd8b49b8bb6c33d26c6ea5dd3d5c43e
jswladitya <jswladitya0403@gmail.com>
2024-08-15 11:07:26 +0530

    update

--- 13_classes_and_oop/6_inheritance.js
+++ 13_classes_and_oop/6_inheritance.js
@@ -3,15 +3,19 @@ class User {
         this.username = username
     }
 
+    //Method
     logMe(){
         console.log(`USERNAME is ${this.username}`);
     }
 }
 
+
+//Hamare pass user aa chuka he ab hame ise Teacher banana he
 class Teacher extends User{
     constructor(username, email, password){
         super(username)
-        //super keyword refers ki jo class ko aap extend kar rahe the us class ke ander jata hu apne this ko bhi le jata hu waha pe jake me username set kar dunga aur aap directly uska eccess bhi le payenge yaha pe
+        //super keyword refers ki jo class ko aap extend kar rahe the ie.., User us class ke ander jata hu apne this ko bhi le jata hu waha pe jake me username set kar dunga waha pe aur aap directly uska eccess bhi le payenge yaha pe
+        //means ab hame call ka concept use nahi karna padega
         this.email=email
         this.password=password
     }
@@ -26,7 +30,7 @@ const chai = new Teacher("chai","chai@teacher.com", "123" )
 chai.addCourse()
 
 const masalaChai= new User("masalaChai")
-masalaChai.addCourse() //iske pass addCourse ka access nahi he kyuki ye alag class me he
+masalaChai.addCourse() //iske pass addCourse ka access nahi he kyuki ye alag class me he parent class he islie aur parent class ke pass child class ka access nahi hota
 
 //we can say equal nahi he chai aur masalaChai but chai instance he Teacher ka
 console.log(chai instanceof Teacher); //true
